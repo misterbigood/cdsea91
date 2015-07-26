bbd00381389c08cf22829a18df485ad7ce291f69
C. Violeau (MDF) <c.violeau@mdfconseil.fr>
2015-07-26 12:35:43 +0200

    Rationalisation des pages Etape 2 - Toutes pages
    Suppression du lien 'Offres d'emploi'  dans aside-content.php

--- inc/css-includes.php
+++ inc/css-includes.php
@@ -1,5 +1,5 @@
 <?php
-if($_SERVER['SERVER_NAME'] == 'localhost') { ?>
+//if($_SERVER['SERVER_NAME'] == 'localhost') { ?>
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/main.css">
 	<link rel="stylesheet" href="css/menu.css">
@@ -7,7 +7,7 @@ if($_SERVER['SERVER_NAME'] == 'localhost') { ?>
 	<link rel="stylesheet" href="css/responsiveslides.css"  />
 	<link rel="stylesheet" href="css/meanmenu.css">
 <?php
-}
-else { ?>
-	<link type="text/css" rel="stylesheet" href="/min/b=css&amp;f=normalize.css,main.css,menu.css,uniform.default.css,responsiveslides.css,meanmenu.css" />
-<?php } ?>
+//}
+//else { ?>
+	<link type="text/css" rel="stylesheet" href="min/b=css&amp;f=normalize.css,main.css,menu.css,uniform.default.css,responsiveslides.css,meanmenu.css" />
+<?php //} ?>
--- inc/js-includes.php
+++ inc/js-includes.php
@@ -1,5 +1,5 @@
 <?php
-if($_SERVER['SERVER_NAME'] == 'localhost') { ?>
+//if($_SERVER['SERVER_NAME'] == 'localhost') { ?>
 	<script type="text/javascript" src="js/jquery-1.10.0.min.js"></script>
 	<script type="text/javascript" src="js/plugins.js"></script>
 	<script type="text/javascript" src="js/meanMenu/jquery.meanmenu.js"></script>
@@ -9,7 +9,7 @@ if($_SERVER['SERVER_NAME'] == 'localhost') { ?>
 	<script type="text/javascript" src="js/main.js"></script>
 	<script type="text/javascript" src="js/doubleTapToGo.js"></script>
 <?php
-}
-else { ?>
+//}
+//else { ?>
 	<script type="text/javascript" src="/min/b=js&amp;f=jquery-1.10.0.min.js,plugins.js,meanMenu/jquery.meanmenu.js,uniform/jquery.uniform.min.js,responsiveslides.js,ajax-jquery-plugin.js,main.js,doubleTapToGo.js"></script>
-<?php } ?>
+<?php// } ?>
