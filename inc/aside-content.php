9005e270d0c84a08afad3d43f11327a00da47638
chris <chris@192.168.1.11>
2016-12-12 12:19:50 +0100

    Modification affichage offres d'emploi Offres retirées

--- inc/aside-content.php
+++ inc/aside-content.php
@@ -3,11 +3,13 @@
 		<input type="hidden" name="search" value="1">
 		<p id="recherche-form"><input name="query" type="text" size="18" class="recherche-input" placeholder="Rechercher"><button name="envoyer" type="submit" class="loupeBtn"><img src="images/btn-recherche.png" width="30" height="30" alt="rechercher"></button></p>
         </form>
+        <!-- Emplacement offres d'emploi
         <a href="actualites.html"><h3 class="iconimage iconimage-photos">Offres d'emploi</h3></a>
         <ul>
             <li><a href='./documentation/offres/51 - H-F entretien.pdf'>ITEP - H/F d'entretien</a></li>
             <li><a href='./documentation/offres/50 - CDS.pdf'>ITEP - Chef de service</a></li>
         </ul>
+        Fin emplacement offres d'emploi -->
 	<?php if($actualites->nbre() > 0) {
 		echo '<a href="actualites.html"><h3 class="iconimage iconimage-news">actualités</h3></a>' . " \n";
 		$actualites->afficherActusAccueil();
