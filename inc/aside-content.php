ba8eb7c441cbbf46612adb020fb92a04036c3b83
C. Violeau (MDF) <c.violeau@mdfconseil.fr>
2017-02-28 18:26:30 +0100

    Mise en ligne annonces ITEP et MECS

--- inc/aside-content.php
+++ inc/aside-content.php
@@ -3,13 +3,13 @@
 		<input type="hidden" name="search" value="1">
 		<p id="recherche-form"><input name="query" type="text" size="18" class="recherche-input" placeholder="Rechercher"><button name="envoyer" type="submit" class="loupeBtn"><img src="images/btn-recherche.png" width="30" height="30" alt="rechercher"></button></p>
         </form>
-        <!-- Emplacement offres d'emploi
+        <!-- Emplacement offres d'emploi -->
         <a href="actualites.html"><h3 class="iconimage iconimage-photos">Offres d'emploi</h3></a>
         <ul>
-            <li><a href='./documentation/offres/51 - H-F entretien.pdf'>ITEP - H/F d'entretien</a></li>
-            <li><a href='./documentation/offres/50 - CDS.pdf'>ITEP - Chef de service</a></li>
+            <li><a href='./documentation/offres/53 - MECS - Coordinateur.pdf'>MECS - Un(e) coordinateur(rice) d'équipe</a></li>
+            <li><a href='./documentation/offres/52 - ITEP - Educateur.pdf'>ITEP - Educateur spécialisé (H/F)</a></li>
         </ul>
-        Fin emplacement offres d'emploi -->
+        <!-- Fin emplacement offres d'emploi -->
 	<?php if($actualites->nbre() > 0) {
 		echo '<a href="actualites.html"><h3 class="iconimage iconimage-news">actualités</h3></a>' . " \n";
 		$actualites->afficherActusAccueil();
