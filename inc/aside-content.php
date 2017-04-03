2f599941c8b7519b643d8df69726643b7ee971da
chris <chris@192.168.1.11>
2017-04-03 12:53:02 +0200

    Ajout diaporama enfants itep

--- inc/aside-content.php
+++ inc/aside-content.php
@@ -3,12 +3,12 @@
 		<input type="hidden" name="search" value="1">
 		<p id="recherche-form"><input name="query" type="text" size="18" class="recherche-input" placeholder="Rechercher"><button name="envoyer" type="submit" class="loupeBtn"><img src="images/btn-recherche.png" width="30" height="30" alt="rechercher"></button></p>
         </form>
-        <!-- Emplacement offres d'emploi -->
+        <!-- Emplacement offres d'emploi 
         <a href="actualites.html"><h3 class="iconimage iconimage-photos">Offres d'emploi</h3></a>
         <ul>
-            <?php if(date("Y-m-d")<"2017-03-24"):?><li><a href='./documentation/offres/54 - CRE - Chargé ASLL.pdf'>CRE - Chargé(e) d'accompagnement social lié au logement</a></li><?php endif;?>
+            <?php //if(date("Y-m-d")<"2017-03-24"):?><li><a href='./documentation/offres/54 - CRE - Chargé ASLL.pdf'>CRE - Chargé(e) d'accompagnement social lié au logement</a></li><?php //endif;?>
         </ul>
-        <!-- Fin emplacement offres d'emploi -->
+         Fin emplacement offres d'emploi -->
 	<?php if($actualites->nbre() > 0) {
 		echo '<a href="actualites.html"><h3 class="iconimage iconimage-news">actualités</h3></a>' . " \n";
 		$actualites->afficherActusAccueil();
@@ -21,6 +21,8 @@
 	else if($page == 'cdsea-offres_emploi') $class_emploi = ' class="menu-on"';
         else if($page== 'cdsea-documentation')$class_documentation = ' class="menu-on"';
 	?>
+        <h4>Diaporama des <a href="http://www.cdsea91.fr/itep-diaporama-2017.html">dessins des enfants de l'ITEP</a></h4>
+    <br>
 	<a href="contact.html"<?php echo $class_contact; ?>><h3 class="iconimage iconimage-map">accès / contact</h3></a>
         <a href="documentation.html"<?php echo $class_documentation; ?>><h3 class="iconimage iconimage-documentation">documentation</h3></a>  
 </div>
