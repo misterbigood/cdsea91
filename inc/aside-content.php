3e4cbcf0e14abd0d1e262c8b41deabc7bd3ba8e4
Christophe Violeau <Christophe Violeau@DESKTOP-ITBH4BV>
2019-01-12 10:10:16 +0100

    Annonces emploi ITEP et SAIS

--- inc/aside-content.php
+++ inc/aside-content.php
@@ -3,11 +3,30 @@
 		<input type="hidden" name="search" value="1">
 		<p id="recherche-form"><input name="query" type="text" size="18" class="recherche-input" placeholder="Rechercher"><button name="envoyer" type="submit" class="loupeBtn"><img src="images/btn-recherche.png" width="30" height="30" alt="rechercher"></button></p>
         </form>
-        <?php $date_max="2018-07-31"; ?>
-        <?php if (date("Y-m-d") < $date_max): ?>
-        <a href="actualites.html"><h3 class="iconimage iconimage-photos">Offres d'emploi</h3></a>
+        <?php   
+                $date_min[63]="2019-01-11";
+                $date_max[63]="2019-02-01";
+                $date_min[64]="2019-01-11";
+                $date_max[64]="2019-02-01";
+                $date_min[65]="2019-01-11";
+                $date_max[65]="2019-02-01";
+                $date_min[66]="2019-01-18";
+                $date_max[66]="2019-02-09";
+                
+        ?>
+        <?php if ((date("Y-m-d") <= max($date_max)) AND (date("Y-m-d") >= min($date_min)) ): ?>
+        <a href="#"><h3 class="iconimage iconimage-photos">Offres d'emploi</h3></a>
         <ul>
-            <?php if(date("Y-m-d")<$date_max):?><li><a href='./documentation/offres/60 - SAEMF - Educateur spécialisé.pdf'>SAEMF - Travailleur social (ES/AS)</a></li><?php endif;?>
+            <?php if((date("Y-m-d") <= $date_max[63]) AND (date("Y-m-d") >= $date_min[63])):?><li><a href='./documentation/offres/63 - ITEP - Responsable technique, logistique et administratif.pdf'>ITEP - Responsable technique, logistique et administratif (H/F)</a></li><?php endif;?>
+        </ul>
+        <ul>
+            <?php if((date("Y-m-d") <= $date_max[64]) AND (date("Y-m-d") >= $date_min[64])):?><li><a href='./documentation/offres/64 - ITEP - Travailleur social.pdf'>ITEP - Travailleur social (H/F)</a></li><?php endif;?>
+        </ul>
+        <ul>
+            <?php if((date("Y-m-d") <= $date_max[65]) AND (date("Y-m-d") >= $date_min[65])):?><li><a href='./documentation/offres/65 - ITEP - Orthophoniste.pdf'>ITEP - Orthophoniste (H/F)</a></li><?php endif;?>
+        </ul>
+         <ul>
+            <?php if((date("Y-m-d") <= $date_max[66]) AND (date("Y-m-d") >= $date_min[66])):?><li><a href='./documentation/offres/66 - SAIS - Psychologue.pdf'>SAIS - Psychologue (H/F)</a></li><?php endif;?>
         </ul>
         <?php endif; ?>
 	<?php if($actualites->nbre() > 0) {
@@ -22,11 +41,8 @@
 	else if($page == 'cdsea-offres_emploi') $class_emploi = ' class="menu-on"';
         else if($page== 'cdsea-documentation')$class_documentation = ' class="menu-on"';
 	?>
-        <h4>Le SAEMF donne la parole aux parents: <a href="http://www.cdsea91.fr/aed-media.html">quatre extraits</a> ou <a class="video" href="https://www.youtube.com/watch?v=1d84QiLy5QU&t=192s&list=PLlCxHYFy8GZFE96RSB4bHYoXnXp4_y_iY&index=2">Paroles de parents - Film complet</a>
-                                                    </h4>
-        <br><h4>Diaporama des <a href="http://www.cdsea91.fr/itep-diaporama-2017.html">dessins des enfants de l'ITEP</a></h4>
+        <h4>Diaporama des <a href="http://www.cdsea91.fr/itep-diaporama-2017.html">dessins des enfants de l'ITEP</a></h4>
     <br>
-        
 	<a href="contact.html"<?php echo $class_contact; ?>><h3 class="iconimage iconimage-map">accès / contact</h3></a>
         <a href="documentation.html"<?php echo $class_documentation; ?>><h3 class="iconimage iconimage-documentation">documentation</h3></a>  
 </div>
