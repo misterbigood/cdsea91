b399df272865b827304ccf473bb6212f00fd38f2
Christophe Violeau <Christophe Violeau@DESKTOP-ITBH4BV>
2020-02-20 12:44:29 +0100

    3ème

--- inc/aside-content.php
+++ inc/aside-content.php
@@ -5,32 +5,16 @@
         </form>
         <?php   
                 $date_min[74]="2020-01-05";
-<<<<<<< HEAD
                 $date_max[74]="2020-03-15";
                 $date_min[76]="2020-02-16";
                 $date_max[76]="2020-03-15";
-=======
-                $date_max[74]="2020-02-09";
-                $date_min[75]="2020-01-18";
-                $date_max[75]="2020-02-15";
->>>>>>> 407e6c1184ba8dbbfa3dd8c0f3d0447770d0b683
                 
         ?>
         <?php if ((date("Y-m-d") <= max($date_max)) AND (date("Y-m-d") >= min($date_min)) ): ?>
         <a href="#"><h3 class="iconimage iconimage-photos">Offres d'emploi</h3></a>
         <ul>
-<<<<<<< HEAD
             <?php if((date("Y-m-d") <= $date_max[74]) AND (date("Y-m-d") >= $date_min[74])):?><li><a href='./documentation/offres/74 - SAEMF - Educateur spécialisé.pdf'>SAEMF - Educateur spécialisé (H/F) - CDD</a></li><?php endif;?>
             <?php if((date("Y-m-d") <= $date_max[76]) AND (date("Y-m-d") >= $date_min[76])):?><li><a href='./documentation/offres/76 - MECS - Secrétaire.pdf'>MECS - Secrétaire - 50% ETP - CDI</a></li><?php endif;?>
-=======
-            <?php if((date("Y-m-d") <= $date_max[72]) AND (date("Y-m-d") >= $date_min[72])):?><li><a href='./documentation/offres/72 - ITEP - Psychologue.pdf'>ITEP - Psychologue (H/F) - 50% ETP - CDI</a></li><?php endif;?>
-        
-            <?php if((date("Y-m-d") <= $date_max[73]) AND (date("Y-m-d") >= $date_min[73])):?><li><a href='./documentation/offres/73 - ITEP - Psychomotricien.pdf'>ITEP - Psychomotricien (H/F) - CDI</a></li><?php endif;?>
-        
-            <?php if((date("Y-m-d") <= $date_max[74]) AND (date("Y-m-d") >= $date_min[74])):?><li><a href='./documentation/offres/74 - SAEMF - Educateur spécialisé.pdf'>SAEMF - Educateur spécialisé (H/F) - CDD</a></li><?php endif;?>
-
-            <?php if((date("Y-m-d") <= $date_max[75]) AND (date("Y-m-d") >= $date_min[75])):?><li><a href='./documentation/offres/75 - MECS - Educateur spécialisé.pdf'>MECS - Educateur spécialisé (H/F) - CDI</a></li><?php endif;?>
->>>>>>> 407e6c1184ba8dbbfa3dd8c0f3d0447770d0b683
         </ul>
         <?php endif; ?>
 	<?php if($actualites->nbre() > 0) {
