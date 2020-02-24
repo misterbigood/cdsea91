82299087d8818145cf0bc01f8fd31064a40ae4e6
Christophe Violeau <Christophe Violeau@DESKTOP-ITBH4BV>
2020-02-24 10:42:40 +0100

    Actualisations annonces SAEMF

--- inc/aside-content.php
+++ inc/aside-content.php
@@ -4,17 +4,23 @@
 		<p id="recherche-form"><input name="query" type="text" size="18" class="recherche-input" placeholder="Rechercher"><button name="envoyer" type="submit" class="loupeBtn"><img src="images/btn-recherche.png" width="30" height="30" alt="rechercher"></button></p>
         </form>
         <?php   
-                $date_min[74]="2020-01-05";
-                $date_max[74]="2020-03-15";
+               
                 $date_min[76]="2020-02-16";
                 $date_max[76]="2020-03-15";
-                
+                $date_min[77]="2020-02-24";
+                $date_max[77]="2020-03-31";
+                $date_min[78]="2020-02-24";
+                $date_max[78]="2020-03-31";
+                $date_min[79]="2020-01-05";
+                $date_max[79]="2020-03-31";
         ?>
         <?php if ((date("Y-m-d") <= max($date_max)) AND (date("Y-m-d") >= min($date_min)) ): ?>
         <a href="#"><h3 class="iconimage iconimage-photos">Offres d'emploi</h3></a>
         <ul>
-            <?php if((date("Y-m-d") <= $date_max[74]) AND (date("Y-m-d") >= $date_min[74])):?><li><a href='./documentation/offres/74 - SAEMF - Educateur spécialisé.pdf'>SAEMF - Educateur spécialisé (H/F) - CDD</a></li><?php endif;?>
             <?php if((date("Y-m-d") <= $date_max[76]) AND (date("Y-m-d") >= $date_min[76])):?><li><a href='./documentation/offres/76 - MECS - Secrétaire.pdf'>MECS - Secrétaire - 50% ETP - CDI</a></li><?php endif;?>
+            <?php if((date("Y-m-d") <= $date_max[77]) AND (date("Y-m-d") >= $date_min[77])):?><li><a href='./documentation/offres/77 - SAEMF - Educateur spécialisé.pdf'>SAEMF St-Michel-sur-Orge - Educateur spécialisé (H/F) - CDI</a></li><?php endif;?>
+            <?php if((date("Y-m-d") <= $date_max[78]) AND (date("Y-m-d") >= $date_min[78])):?><li><a href='./documentation/offres/78 - SAEMF - Educateur spécialisé.pdf'>SAEMF Vigneux-sur-Seine - Educateur spécialisé (H/F) - CDI</a></li><?php endif;?>
+            <?php if((date("Y-m-d") <= $date_max[79]) AND (date("Y-m-d") >= $date_min[79])):?><li><a href='./documentation/offres/79 - SAEMF - Educateur spécialisé.pdf'>SAEMF Evry - Educateur spécialisé (H/F) - CDD</a></li><?php endif;?>
         </ul>
         <?php endif; ?>
 	<?php if($actualites->nbre() > 0) {
