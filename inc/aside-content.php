fa0d812c8ba425ca496964c0bf1ff2265880fc6e
Christophe Violeau <Christophe Violeau@DESKTOP-ITBH4BV>
2020-02-25 18:32:34 +0100

    Annonce ITEP

--- inc/aside-content.php
+++ inc/aside-content.php
@@ -13,6 +13,9 @@
                 $date_max[78]="2020-03-31";
                 $date_min[79]="2020-01-05";
                 $date_max[79]="2020-03-31";
+                $date_min[80]="2020-02-25";
+                $date_max[80]="2020-03-31";
+                
         ?>
         <?php if ((date("Y-m-d") <= max($date_max)) AND (date("Y-m-d") >= min($date_min)) ): ?>
         <a href="#"><h3 class="iconimage iconimage-photos">Offres d'emploi</h3></a>
@@ -21,6 +24,7 @@
             <?php if((date("Y-m-d") <= $date_max[77]) AND (date("Y-m-d") >= $date_min[77])):?><li><a href='./documentation/offres/77 - SAEMF - Educateur spécialisé.pdf'>SAEMF St-Michel-sur-Orge - Educateur spécialisé (H/F) - CDI</a></li><?php endif;?>
             <?php if((date("Y-m-d") <= $date_max[78]) AND (date("Y-m-d") >= $date_min[78])):?><li><a href='./documentation/offres/78 - SAEMF - Educateur spécialisé.pdf'>SAEMF Vigneux-sur-Seine - Educateur spécialisé (H/F) - CDI</a></li><?php endif;?>
             <?php if((date("Y-m-d") <= $date_max[79]) AND (date("Y-m-d") >= $date_min[79])):?><li><a href='./documentation/offres/79 - SAEMF - Educateur spécialisé.pdf'>SAEMF Evry - Educateur spécialisé (H/F) - CDD</a></li><?php endif;?>
+            <?php if((date("Y-m-d") <= $date_max[80]) AND (date("Y-m-d") >= $date_min[80])):?><li><a href='./documentation/offres/80 - ITEP - Educateur spécialisé.pdf'>ITEP - Educateur spécialisé (H/F) - CDI</a></li><?php endif;?>
         </ul>
         <?php endif; ?>
 	<?php if($actualites->nbre() > 0) {
